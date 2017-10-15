a9bf7610d8da3a992897d8c04f97b7bd01a2110f
sriramchaudhury <sriramchaudhury@gmail.com>
2017-10-15 23:57:04 +0530

    added csv-generator

---
+++ csv-generator/csv-generator.py
@@ -0,0 +1,85 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#######################################################################
+#                               content                               #
+
+
+# Objective
+    
+#    Given the csv file for a source language and morphological analyser for a target language
+#    
+#    generate following files for the target language.
+#
+#    
+#        1.  CSV file
+#        2.  Concept-dictionary
+#        3.  karaka-vibhakti mapping
+#
+#Logic:
+#
+#1. Read Hindi and Telugu parallel sentence and map the words in the sentences.
+#
+#2. Run the telugu morph on each word of the Telugu sentence
+#
+#	2.1. mark the root, vibhakti, tam for each word
+#
+#3. start writing the Telugu csv file similar to the Hindi csv rows.
+
+#
+
+#Functions:
+#
+#    1. Create_concept_dictionary
+#    2. Create_karaka_vibhakti_mapping
+#    3. create_csv
+#
+
+
+#
+#Assumption: 
+#
+#Hindi and Telugu sentences are parallel and word aligned (i.e same number of words in Hindi and Telugu)
+#
+#
+#Required files/tools:
+#
+#1. Hindi csv file
+#2. Hindi - Telugu parallel sentences
+#3. Telugu morph analyser
+#
+#
+
+
+#######################################################################
+
+#read the hindi csv file
+
+
+
+#read the Hindi Telugu parallel sentences
+
+# run morph on Telugu words
+
+# create csv file
+
+# Create Concept dictionary
+
+
+# create karaka-vibhakti mapping
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
